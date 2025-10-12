74bb245e1e918fa50dd3145644bc6e0a5fb30800
OfirTzrik <ofirc36@gmail.com>
2025-10-12 20:49:21 +0300

    problem3 solution and renames

---
+++ problem3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    long int largest_factor = 0;
+    long int target = 600851475143;
+    long int target_sqrt = std::sqrt(target);
+    long int factor = 2;
+    
+    while (target > 0 && factor < target_sqrt) {
+        if (!(target % factor)) {
+            target /= factor;
+            largest_factor = factor > largest_factor ? factor : largest_factor;
+        } else {
+            factor++;
+        }
+    }
+
+    std::cout << largest_factor << "\n";
+}
