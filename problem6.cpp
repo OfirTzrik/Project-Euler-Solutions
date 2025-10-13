61387f83e1f1b5c3ee6a52b294dbe9bf13fd84b8
OfirTzrik <ofirc36@gmail.com>
2025-10-13 20:50:07 +0300

    problems 6 and 7 solutions

---
+++ problem6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  int sum_squares { 0 };
+  int square_sums { 0 };
+
+  for (int i = 0; i <= 100; i++) {
+    sum_squares += i * i;
+    square_sums += i;
+  }
+  
+  std::cout << (square_sums * square_sums) - sum_squares << "\n";
+
+  return 0;
+}
