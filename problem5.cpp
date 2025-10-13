a950efed2f7b218f690119db47e6e7b450737a01
OfirTzrik <ofirc36@gmail.com>
2025-10-13 14:49:38 +0300

    problem5 solution

---
+++ problem5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+	int i { 19 };
+
+	while (i++) {
+		for (int j = 2; j <= 20; j++) {
+			if (i % j) {
+				break;
+			}
+			if (j == 20) {
+				goto done;
+			}
+		}
+	}
+
+done:	std::cout << i << "\n";
+	return 0;
+}
