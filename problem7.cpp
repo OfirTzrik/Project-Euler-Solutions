680697233cdbc4af075d738ac849942c6f217dd3
OfirTzrik <ofirc36@gmail.com>
2025-10-13 20:51:56 +0300

    fix indentation

--- problem7.cpp
+++ problem7.cpp
@@ -2,26 +2,26 @@
 #include <cmath>
 
 bool is_prime(int num) {
-  for (int i = 2; i <= std::sqrt(num); i++) {
-    if (!(num % i)) {
-      return 0;
+    for (int i = 2; i <= std::sqrt(num); i++) {
+      if (!(num % i)) {
+        return 0;
+      }
     }
-  }
-  return 1;
+    return 1;
 }
 
 int main() {
-  int num_primes { 0 };
-  int curr_num { 2 };
-  
-  while (num_primes < 10001) {
-    if (is_prime(curr_num)) {
-      num_primes++;
+    int num_primes { 0 };
+    int curr_num { 2 };
+
+    while (num_primes < 10001) {
+      if (is_prime(curr_num)) {
+        num_primes++;
+      }
+      curr_num++;
     }
-    curr_num++;
-  }
-  
-  std::cout << --curr_num << "\n";
-  
-  return 0;
+
+    std::cout << --curr_num << "\n";
+
+    return 0;
 }
