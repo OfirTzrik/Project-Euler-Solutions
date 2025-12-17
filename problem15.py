9089e83838e6ce3e504cd9755f49666dcfdea341
OfirTzrik <ofirc36@gmail.com>
2025-12-17 20:52:01 +0200

    small changes to problem 15

--- problem15.py
+++ problem15.py
@@ -2,5 +2,8 @@ import math
 
 # Mark step right as R and step down as D
 # This problem describes the number of possible ways to order 20 Rs and 20 Ds without repititions
-num_paths = math.factorial(40) / (math.factorial(20) * math.factorial(20))
+size_x = 20
+size_y = 20
+
+num_paths = math.factorial(size_x + size_y) / (math.factorial(size_x) * math.factorial(size_y))
 print(num_paths)
