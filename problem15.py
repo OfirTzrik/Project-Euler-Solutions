b1f9fe09f4cff2c345195e262314cc418eb136d4
OfirTzrik <72357336+OfirTzrik@users.noreply.github.com>
2025-12-21 16:19:17 +0200

    Fix typo in comment about path ordering

--- problem15.py
+++ problem15.py
@@ -1,7 +1,7 @@
 import math
 
 # Mark step right as R and step down as D
-# This problem describes the number of possible ways to order 20 Rs and 20 Ds without repititions
+# This problem describes the number of possible ways to order 20 Rs and 20 Ds without repetitions
 size_x = 20
 size_y = 20
 
